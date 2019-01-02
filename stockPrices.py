37bbafb8740753b2e9d2446d1a7b88cee6a17db2
Alex <Sunny.hsia1785@gmail.com>
2019-01-03 06:00:23 +0800

    Update stockPrices.py

--- stockPrices.py
+++ stockPrices.py
@@ -5,9 +5,10 @@ sourceCode = str(r.content)
 
 def getOpen(date):
     dateIndex = sourceCode.find(date)
-    #Stock price is located 131 characters after the date for each date
+    #Open price is located 131 characters after the date for each date
     return(sourceCode[dateIndex + 131:dateIndex + 137])
 
 def getClose(date):
     dateIndex = sourceCode.find(date)
+    #Close price is located 131 characters after the date for each date
     return(sourceCode[dateIndex + 512:dateIndex + 518])
