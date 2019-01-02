dd7024d566c8d4f86e7025f3335bd3427557cc17
gemeLn <mtang317@gmail.com>
2019-01-02 15:14:31 -0600

    3:14

--- twitterStocks.py
+++ twitterStocks.py
@@ -1,15 +1,16 @@
 import math
 import random
 import tweepy
-#Key dictionary
-keys={}
-#Grabbing keys
-keyfile = open('keys.secret','r').read().splitlines()
+
+# Key dictionary
+keys = {}
+# Grabbing keys
+keyfile = open('keys.secret', 'r').read().splitlines()
 for line in keyfile:
-	pair = line.split(':')
-	keys[pair[0]]=pair[1]
-#Init twitter bot
-auth = tweepy.OAuthHandler(keys["apikey"],keys["apisecret"])
-auth.set_access_token(keys["accesstoken"],keys["accesssecret"])
-api=tweepy.API(auth)
-api.update_status("OK")
+    pair = line.split(':')
+    keys[pair[0]] = pair[1]
+# Init twitter bot
+auth = tweepy.OAuthHandler(keys["apikey"], keys["apisecret"])
+auth.set_access_token(keys["accesstoken"], keys["accesssecret"])
+api = tweepy.API(auth)
+api.update_status("OK2")
