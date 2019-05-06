e9d9f38da516a74089ef918b04177732a279c078
gemeLn <mtang317@gmail.com>
2019-05-06 06:38:12 -0500

    Matty 5/6 6:38

--- NeuralNetMatty.py
+++ NeuralNetMatty.py
@@ -1,10 +1,14 @@
-from builtins import enumerate
 import numpy as np
 import torch
-from torch.autograd import Variable
 import pandas as pd
 
+seed = 1  # Sample seed
 df = pd.read_csv("data/AAPL60.csv")
+df["indicies"] = df.index
+train = df.sample(frac=0.9, replace=True, random_state=seed)
+print(train[0]['indicies'])
+exit()
+test = df.loc[~df.index.isin(train.index), :]
 s = df['close']
 l = len(s)
 j = 100  # Use past j days as predictor
@@ -14,17 +18,11 @@ for i in range(0, l - j):
     xtemp.append(np.array(s[i:i + j].values))
     ytemp.append(s.get(i + j))
 
-xdata = np.array(xtemp, dtype=np.float32)
-ydata = np.array(ytemp, dtype=np.float32)
-ydata = ydata.reshape(-1,1)
-xdata=xdata/300
-ydata=ydata/300
-# x_values = [i for i in range(11)]
-# x_train = np.array(x_values, dtype=np.float32)
-# x_train = x_train.reshape(-1, 1)
-# y_values = [2 * i + 1 for i in x_values]
-# y_train = np.array(y_values, dtype=np.float32)
-# y_train = y_train.reshape(-1, 1)
+trainx = np.array(xtemp, dtype=np.float32)
+trainy = np.array(ytemp, dtype=np.float32)
+trainy = trainy.reshape(-1, 1)
+trainx = trainx / 300
+trainy = trainy / 300
 
 
 class LinearRegressionModel(torch.nn.Module):
@@ -41,8 +39,8 @@ model = LinearRegressionModel(j, 1)
 critereon = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 for epoch in range(500):
-    inputs = torch.from_numpy(xdata).requires_grad_()
-    labels = torch.from_numpy(ydata)
+    inputs = torch.from_numpy(trainx).requires_grad_()
+    labels = torch.from_numpy(trainy)
     optimizer.zero_grad()
     outputs = model(inputs)
     loss = critereon(outputs, labels)
